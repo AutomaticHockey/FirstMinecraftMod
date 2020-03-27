5ba39bb27b02e288ef72b4a5ac6401a0131756fc
Lucas Anderson <lucasjwanderson@gmail.com>
2020-03-26 17:24:00 -0700

    First Commit

---
+++ src/main/java/com/example/examplemod/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package com.example.examplemod.util;
+
+import com.example.examplemod.RainbowPoopSwordMod;
+import com.example.examplemod.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, RainbowPoopSwordMod.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+}
